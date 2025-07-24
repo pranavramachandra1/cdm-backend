116483dfe4b971ab860b34062cab2aba0991e499
pranavramachandra1 <pranavramachandra@berkeley.edu>
2025-07-23 22:30:08 -0400

    didnt remove breakpoints wtf

--- app/dependencies.py
+++ app/dependencies.py
@@ -28,8 +28,6 @@ def get_mongo_db():
         server_api=ServerApi("1"),
     )
 
-    breakpoint()
-
     # Return DB
     return mongo_url.todov2
 
@@ -44,8 +42,6 @@ def get_user_service() -> UserService:
     user_collection = (
         db["test-users"] if os.getenv("ENV") == TEST_ENV else db["prod-users"]
     )
-
-    breakpoint()
 
     return UserService(user_collection=user_collection)
 
@@ -88,7 +84,6 @@ def get_task_service() -> TaskService:
         list_collection=list_collection,
         task_collection=task_collection,
     )
-
 
 if __name__ == "__main__":
 
